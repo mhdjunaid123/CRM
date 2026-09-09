d4eb211bbab3b69d607d5f89fa082f18027f1922
mhdjunaid123 <mhdjunaidd62562@gmail.com>
2026-09-09 11:26:12 +0530

    Add additional origin to CORS middleware

--- backend/server.py
+++ backend/server.py
@@ -1351,7 +1351,11 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=[os.environ.get("FRONTEND_URL", "http://localhost:3000"), "http://localhost:3000"],
+    allow_origins=[
+        os.environ.get("FRONTEND_URL", "http://localhost:3000"),
+        "http://localhost:3000",
+        "https://crmfrontend-nine.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
